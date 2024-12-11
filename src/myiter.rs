3fd062a06ee5bb5bbf639ff5cc59690d5ed7cdf8
Jim124 <fengpingan065@gmail.com>
2024-12-11 11:53:44 +0000

    iterator chain method

--- src/myiter.rs
+++ src/myiter.rs
@@ -1,11 +1,15 @@
 pub fn test_iterator(){
   let fruit_list = vec!["Apple","Strawberry","Blueberry","Orange"];
   let mut  fruit_iterator = fruit_list.iter();
-
+  let nuts_list = vec!["penuts","almonds","walsnuts"];
   fruit_iterator.next();
   fruit_iterator.next();
   let fruit = fruit_iterator.next();
-  println!("{}",fruit.unwrap() );
+  println!("{}",fruit.unwrap());
+  let aggregrate_list = fruit_list.iter().chain(&nuts_list);
+  for food in aggregrate_list{
+    println!("{}",food );
+  }
   // for fruit in fruit_iterator{
   //   println!("{}", fruit);
   // }
